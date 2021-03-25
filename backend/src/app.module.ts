131cb7dd1fe4b5d84555f4d1edffed7e92bb7869
yee2542 <yee2542gtl@gmail.com>
2021-03-25 19:34:07 +0700

    preventing from bad env path

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppResolver } from './app.resolver';
 import { AppService } from './app.service';
@@ -7,7 +8,7 @@ import { AppService } from './app.service';
 @Module({
   imports: [
     GraphQLModule.forRoot({
-      autoSchemaFile: 'schema.gql',
+      autoSchemaFile: join(process.cwd(), 'schema.gql'),
       sortSchema: true,
     }),
   ],
