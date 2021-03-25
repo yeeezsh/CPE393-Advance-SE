e9f9e1fe07738250b212be990b331b9735e1896e
Thaweesak Saiwongse <57885091+Thaweesak-sai@users.noreply.github.com>
2021-03-25 18:21:20 +0700

    Remove unused imports

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,10 +1,7 @@
-import { toUserDto } from 'src/shared/mapper';
 import { CreateUserDto } from './user.create.dto';
 import { UserDto } from './user.dto';
-import { UserEntity } from './user.entity';
 
 export class UserService {
-  constructor() {}
 
   async createUser(userDto: CreateUserDto): Promise<UserDto> {
     const { username, password, email } = userDto;
