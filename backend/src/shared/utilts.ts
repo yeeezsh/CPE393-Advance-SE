18eba0af27bec4459320ef9a148f1388e06180bc
Thaweesak Saiwongse <57885091+Thaweesak-sai@users.noreply.github.com>
2021-03-25 18:58:04 +0700

    fix style

--- backend/src/shared/utilts.ts
+++ backend/src/shared/utilts.ts
@@ -3,6 +3,4 @@ import * as bcrypt from 'bcrypt';
 export const comparePassword = async (
   userPassword: string,
   currentPassword: string,
-) => {
-  return await bcrypt.compare(currentPassword, userPassword);
-};
+) => await bcrypt.compare(currentPassword, userPassword);
