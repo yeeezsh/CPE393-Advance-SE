4a3f286d270c935343c7d858dcfa97ab503d56a9
yee2542 <yee2542gtl@gmail.com>
2021-03-26 14:14:38 +0700

    add config provider

---
+++ backend/src/config/config.providers.ts
@@ -0,0 +1,32 @@
+import { Provider } from '@nestjs/common';
+import { CONFIG_PROVIDER } from './@types/config.constant';
+import { ConfigEnvType } from './@types/config.env';
+import { ConfigType } from './@types/config.type';
+
+export const configProviders: Provider[] = [
+  {
+    provide: CONFIG_PROVIDER,
+    useValue: (): ConfigType => {
+      const {
+        DATABASE_CONNECTION,
+        DATABASE_USERNAME,
+        DATABASE_PASSWORD,
+        DATABASE_AUTH_SOURCE,
+        PORT,
+        ORIGIN,
+        TZ,
+      } = process.env as ConfigEnvType;
+      return {
+        database: {
+          connection_string: DATABASE_CONNECTION,
+          username: DATABASE_USERNAME,
+          password: DATABASE_PASSWORD,
+          auth_source: DATABASE_AUTH_SOURCE,
+        },
+        port: Number(PORT),
+        origin: ORIGIN,
+        timezone: TZ,
+      };
+    },
+  },
+];
