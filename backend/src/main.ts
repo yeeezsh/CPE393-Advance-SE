9d0bd49bc52f316419af5f54edf05742aa070b7b
yee2542 <yee2542gtl@gmail.com>
2021-03-26 14:22:37 +0700

    implement config

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CONFIG_PROVIDER } from './config/@types/config.constant';
+import { ConfigType } from './config/@types/config.type';
+import { ConfigModule } from './config/config.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const { port }: ConfigType = app.select(ConfigModule).get(CONFIG_PROVIDER);
+
+  await app.listen(port);
 }
 bootstrap();
