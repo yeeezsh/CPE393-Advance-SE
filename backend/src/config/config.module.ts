e62141b51048ac8eec8bf2df20ed80a6e6c48ea6
yee2542 <yee2542gtl@gmail.com>
2021-03-26 14:22:18 +0700

    refactor

--- backend/src/config/config.module.ts
+++ backend/src/config/config.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
-import { configProviders } from './config.providers';
+import { configProvider } from './config.provider';
 
 @Module({
-  providers: [...configProviders],
-  exports: [...configProviders],
+  providers: [configProvider],
+  exports: [configProvider],
 })
 export class ConfigModule {}
