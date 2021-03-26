0c814e1434a5d8fd9123b611c3a1448ba9f8f823
yee2542 <yee2542gtl@gmail.com>
2021-03-26 14:14:04 +0700

    refactor

--- backend/src/config/@types/config.env.ts
+++ backend/src/config/@types/config.env.ts
@@ -1,8 +1,13 @@
-export enum ConfigEnum {
+export enum ConfigEnvEnum {
   DATABASE_CONNECTION = 'DATABASE_CONNECTION',
   DATABASE_USERNAME = 'DATABASE_USERNAME',
   DATABASE_PASSWORD = 'DATABASE_PASSWORD',
   DATABASE_AUTH_SOURCE = 'DATABASE_AUTH_SOURCE',
   ORIGIN = 'ORIGIN',
   PORT = 'PORT',
+  TZ = 'TZ',
 }
+
+export type ConfigEnvType = {
+  [key in ConfigEnvEnum]: string;
+};
