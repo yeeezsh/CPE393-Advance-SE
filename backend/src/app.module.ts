9d38e06383ab76b9d5050bf7cc0cb68bf00271ed
yee2542 <yee2542gtl@gmail.com>
2021-03-28 14:52:36 +0700

    register mongoose for root

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,14 +1,17 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { MongooseModule } from '@nestjs/mongoose';
 import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppResolver } from './app.resolver';
 import { AppService } from './app.service';
+import { ConfigDatabaseService } from './config/config.database.service';
 import { ConfigModule } from './config/config.module';
 
 @Module({
   imports: [
     ConfigModule,
+    MongooseModule.forRootAsync({ useClass: ConfigDatabaseService }),
     GraphQLModule.forRoot({
       autoSchemaFile: join(process.cwd(), 'schema.gql'),
       sortSchema: true,
