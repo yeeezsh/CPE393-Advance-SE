72a969babc987fb5fda2b2b4f41031cb37fa774d
yee2542 <yee2542gtl@gmail.com>
2021-03-28 17:39:32 +0700

    register account module

--- backend/src/account/account.module.ts
+++ backend/src/account/account.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from './schema/user.schema';
 
-@Module({})
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
+})
 export class AccountModule {}
