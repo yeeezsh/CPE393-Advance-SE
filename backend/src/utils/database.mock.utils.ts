d56614c881200b29fdb0ac486de0efa9a9810732
yee2542 <yee2542gtl@gmail.com>
2021-03-28 17:07:16 +0700

    deprecated new db instance

--- backend/src/utils/database.mock.utils.ts
+++ backend/src/utils/database.mock.utils.ts
@@ -2,13 +2,9 @@ import { Logger } from '@nestjs/common';
 import { MongoMemoryReplSet } from 'mongodb-memory-server';
 import { ConfigAppServiceType } from '../config/@types/config-app.service.type';
 
-export const replSet = new MongoMemoryReplSet({
-  replSet: { storageEngine: 'wiredTiger' },
-});
-
-export const databaseMockUtils = async (): Promise<
-  ConfigAppServiceType['database']
-> => {
+export const databaseMockUtils = async (
+  replSet: MongoMemoryReplSet,
+): Promise<ConfigAppServiceType['database']> => {
   await replSet.waitUntilRunning();
   const uri = await replSet.getUri();
   const onJest = process.env.JEST_WORKER_ID !== undefined;
