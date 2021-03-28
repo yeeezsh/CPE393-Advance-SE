e32b46d31cb9179189f260375a88cf82ce8cc7a8
yee2542 <yee2542gtl@gmail.com>
2021-03-28 16:37:33 +0700

    implement db mock

--- backend/test/app.e2e-spec.ts
+++ backend/test/app.e2e-spec.ts
@@ -1,10 +1,24 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
 import request from 'supertest';
+import { ConfigEnvType } from '../src/config/@types/config.env.type';
+import { databaseMockUtils } from '../src/utils/database.mock.utils';
 import { AppModule } from './../src/app.module';
+import { MOCK_ENV } from './mock.env.constant';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+
+  beforeAll(async () => {
+    const db = await databaseMockUtils();
+    process.env = {
+      ...MOCK_ENV,
+      DATABASE_CONNECTION: db.connection_string,
+      DATABASE_USERNAME: db.username,
+      DATABASE_PASSWORD: db.password,
+      DATABASE_AUTH_SOURCE: db.auth_source,
+    } as ConfigEnvType;
+  });
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
