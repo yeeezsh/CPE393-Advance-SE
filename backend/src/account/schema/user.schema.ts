16be653206633849222ae49411d7e0f9ac257d36
yee2542 <yee2542gtl@gmail.com>
2021-03-28 17:36:04 +0700

    add user schema

---
+++ backend/src/account/schema/user.schema.ts
@@ -0,0 +1,24 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type UserDocument = User & Document;
+
+@Schema()
+export class User {
+  @Prop()
+  displayName: string;
+
+  @Prop({ required: true })
+  username: string;
+
+  @Prop({ required: true })
+  password: string;
+
+  @Prop({ required: true })
+  email: string;
+
+  @Prop({ required: true, default: Date.now })
+  createAt: Date;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
