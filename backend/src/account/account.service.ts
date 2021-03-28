090916a3efcbc6b29cca4a8d8385389666a5a450
yee2542 <yee2542gtl@gmail.com>
2021-03-28 18:07:40 +0700

    add sample acc service

--- backend/src/account/account.service.ts
+++ backend/src/account/account.service.ts
@@ -6,4 +6,13 @@ import { User, UserDocument } from './schema/user.schema';
 @Injectable()
 export class AccountService {
   constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+
+  async createAccount(): Promise<void> {
+    await this.userModel.create({
+      username: '',
+      password: '',
+      email: '',
+    });
+    return;
+  }
 }
