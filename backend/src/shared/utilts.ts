94c5385a0ae3362ece31aced3d122516ae37a2b7
yee2542 <yee2542gtl@gmail.com>
2021-03-29 17:26:33 +0700

    deprecated user module

--- backend/src/shared/utilts.ts
+++
@@ -1,6 +0,0 @@
-import * as bcrypt from 'bcrypt';
-
-export const comparePassword = async (
-  userPassword: string,
-  currentPassword: string,
-) => await bcrypt.compare(currentPassword, userPassword);
--- backend/src/users/user.dto.ts
+++
@@ -1,7 +0,0 @@
-export class UserDto {
-  id?: string;
-  username?: string;
-  password?: string;
-  email?: string;
-  createdOn?: Date;
-}
