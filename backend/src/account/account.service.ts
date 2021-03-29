0481dddb13c7a9f2e9921a4e9397e22d51dd94d0
yee2542 <yee2542gtl@gmail.com>
2021-03-29 15:20:41 +0700

    implement user exception

--- backend/src/account/account.service.ts
+++ backend/src/account/account.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { UserBadRequestException } from './exceptions/user.bad-request.exception';
+import { UserForbiddenException } from './exceptions/user.forbidden.exception';
 import { User, UserDocument } from './schema/user.schema';
 
 @Injectable()
@@ -18,11 +20,12 @@ export class AccountService {
   }
 
   async deactivateAccount(userId: UserDocument['_id']): Promise<UserDocument> {
+    if (!userId) throw new UserBadRequestException();
     const updated = await this.userModel.findOneAndUpdate(
       { _id: userId },
       { deactivate: true },
     );
-    if (!updated) throw new Error('user not exisiting');
+    if (!updated) throw new UserForbiddenException();
     return updated;
   }
 }
