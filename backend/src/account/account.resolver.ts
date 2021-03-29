e0fbf092577b552fd90cad0d71304d272c376e31
yee2542 <yee2542gtl@gmail.com>
2021-03-29 18:00:06 +0700

    implement user mongo error filter

--- backend/src/account/account.resolver.ts
+++ backend/src/account/account.resolver.ts
@@ -1,13 +1,16 @@
+import { UseFilters } from '@nestjs/common';
 import { Args, Mutation, Resolver } from '@nestjs/graphql';
 import { AccountService } from './account.service';
 import { UserDTO } from './dtos/user.dto';
 import { UserRegisterInputDTO } from './dtos/user.register.input.dto';
 import { UserResponseDTO } from './dtos/user.response.dto';
+import { UserMongoErrorFilterException } from './exceptions/filters/user.mongo-error.filter-exception';
 
 @Resolver(() => UserDTO)
 export class AccountResolver {
   constructor(private accountService: AccountService) {}
 
+  @UseFilters(UserMongoErrorFilterException)
   @Mutation(() => UserResponseDTO)
   async createAccount(
     @Args('UserRegisterInputDTO') userRegisterInputDTO: UserRegisterInputDTO,
