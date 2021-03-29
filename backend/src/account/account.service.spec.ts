f8b509c81372641dc79591af74e384a1995919d6
yee2542 <yee2542gtl@gmail.com>
2021-03-29 15:09:44 +0700

    add test fot deactivate acc

--- backend/src/account/account.service.spec.ts
+++ backend/src/account/account.service.spec.ts
@@ -1,30 +1,44 @@
+import { Provider, ValueProvider } from '@nestjs/common';
 import { getModelToken } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
+import { Types } from 'mongoose';
 import { AccountService } from './account.service';
 import { User, UserDocument } from './schema/user.schema';
 
 const MOCK_USER_DOCUMENT = {
+  _id: Types.ObjectId(),
   displayName: 'ds',
   username: '',
   email: 'a@b.c',
-} as Omit<UserDocument, 'password'>;
+  password: '1234',
+} as UserDocument;
 
 describe('AccountService', () => {
   let provider: AccountService;
+  let mockUserModel: Provider;
+  let mockUserUseValue: {
+    create: jest.Mock<any, any>;
+    findOneAndUpdate: jest.Mock<any, any>;
+  };
+
+  beforeAll(() => {
+    mockUserUseValue = {
+      create: jest
+        .fn()
+        .mockImplementation(() => Promise.resolve(MOCK_USER_DOCUMENT)),
+      findOneAndUpdate: jest
+        .fn()
+        .mockImplementation(() => Promise.resolve(MOCK_USER_DOCUMENT)),
+    };
+    mockUserModel = {
+      provide: getModelToken(User.name),
+      useValue: mockUserUseValue,
+    };
+  });
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [
-        {
-          provide: getModelToken(User.name),
-          useValue: {
-            create: jest
-              .fn()
-              .mockImplementation(() => Promise.resolve(MOCK_USER_DOCUMENT)),
-          },
-        },
-        AccountService,
-      ],
+      providers: [mockUserModel, AccountService],
     }).compile();
 
     provider = module.get<AccountService>(AccountService);
@@ -37,5 +51,18 @@ describe('AccountService', () => {
   it('should able to create and return user data', async () => {
     const createAccount = await provider.createAccount();
     expect(createAccount).toEqual(MOCK_USER_DOCUMENT);
+  });
+
+  it('should able to deactivate and throw if user not exisiting', async () => {
+    const deactivate = await provider.deactivateAccount(MOCK_USER_DOCUMENT._id);
+    expect(deactivate).toEqual(MOCK_USER_DOCUMENT);
+
+    jest.spyOn(mockUserUseValue, 'findOneAndUpdate').mockImplementation(() => {
+      throw new Error('user not exisiting');
+    });
+    const notExisitingObjectId = Types.ObjectId();
+    expect(
+      provider.deactivateAccount(notExisitingObjectId),
+    ).rejects.toThrowError();
   });
 });
