100b01532e221fbd2bdead801cb210f83de2d3a3
yee2542 <korbboon.thuswongsa@mail.kmutt.ac.th>
2021-03-29 15:34:33 +0700

    [OUM-29][BE] Create user model and user repository (#11)
    * add account module
    
    * add user schema
    
    * register account module
    
    * add acc service
    
    * add sample acc service
    
    * add sample createAccount metjod
    
    * add print width
    
    * add deactivate
    
    * add test fot deactivate acc
    
    * org import
    
    * add user forbidden exception
    
    * add user exception
    
    * add common exception
    
    * implement user exception
    
    * implement common exception

---
+++ backend/src/account/schema/user.schema.ts
@@ -0,0 +1,27 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type UserDocument = User & Document;
+
+@Schema()
+export class User {
+  @Prop()
+  displayName: string;
+
+  @Prop({ required: true })
+  username: string;
+
+  @Prop({ required: true })
+  password: string;
+
+  @Prop({ required: true })
+  email: string;
+
+  @Prop({ required: true, default: Date.now })
+  createAt: Date;
+
+  @Prop({ default: undefined })
+  deactivate?: boolean;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
