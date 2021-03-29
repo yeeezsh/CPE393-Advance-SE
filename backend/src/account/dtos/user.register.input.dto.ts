e6558ae4e57e64d48c9e7c0273d634ae80a5ac0f
yee2542 <yee2542gtl@gmail.com>
2021-03-29 17:12:57 +0700

    implement validator

--- backend/src/account/dtos/user.register.input.dto.ts
+++ backend/src/account/dtos/user.register.input.dto.ts
@@ -1,17 +1,24 @@
 import { Field, InputType } from '@nestjs/graphql';
+import { IsEmail, IsString, Max, Min } from 'class-validator';
 import { User } from '../schema/user.schema';
 
 @InputType()
 export class UserRegisterInputDTO implements Omit<User, 'createAt'> {
   @Field()
+  @IsString()
   displayName: string;
 
   @Field()
+  @IsString()
   username: string;
 
   @Field()
+  @IsString()
+  @Min(6)
+  @Max(40)
   password: string;
 
   @Field()
+  @IsEmail()
   email: string;
 }
