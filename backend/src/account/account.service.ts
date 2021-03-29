d6473c62448b03cbf07ac0d44a08b5b039564013
yee2542 <yee2542gtl@gmail.com>
2021-03-29 15:21:40 +0700

    implement common exception

--- backend/src/account/account.service.ts
+++ backend/src/account/account.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { UnExpectedServerException } from '../common/exceptions/un-expected.server.exception';
 import { UserBadRequestException } from './exceptions/user.bad-request.exception';
 import { UserForbiddenException } from './exceptions/user.forbidden.exception';
 import { User, UserDocument } from './schema/user.schema';
@@ -10,22 +11,30 @@ export class AccountService {
   constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
 
   async createAccount(): Promise<UserDocument> {
-    const doc = await this.userModel.create({
-      username: '',
-      password: '',
-      email: '',
-    });
+    try {
+      const doc = await this.userModel.create({
+        username: '',
+        password: '',
+        email: '',
+      });
 
-    return doc;
+      return doc;
+    } catch (err) {
+      throw new UnExpectedServerException(err);
+    }
   }
 
   async deactivateAccount(userId: UserDocument['_id']): Promise<UserDocument> {
     if (!userId) throw new UserBadRequestException();
-    const updated = await this.userModel.findOneAndUpdate(
-      { _id: userId },
-      { deactivate: true },
-    );
-    if (!updated) throw new UserForbiddenException();
-    return updated;
+    try {
+      const updated = await this.userModel.findOneAndUpdate(
+        { _id: userId },
+        { deactivate: true },
+      );
+      if (!updated) throw new UserForbiddenException();
+      return updated;
+    } catch (err) {
+      throw new UnExpectedServerException(err);
+    }
   }
 }
