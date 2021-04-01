f0352135bfca5ecbff89526dbdade460497a223d
wuttithat.krongyot <wuttithat.krongyot@accenture.com>
2021-04-01 17:26:51 +0700

    refactor: rename const

--- frontend/src/pages/signin.tsx
+++ frontend/src/pages/signin.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import Demo from "../modules/user/pages/signin";
+import SignInPage from "../modules/user/pages/signin";
 
 const SignIn: React.FC = () => {
-  return <Demo />;
+  return <SignInPage />;
 };
 
 export default SignIn;
