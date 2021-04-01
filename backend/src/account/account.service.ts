616ab8d804b04fcad57e9d2df0162703fb1304da
yee2542 <yee2542gtl@gmail.com>
2021-04-01 22:18:00 +0700

    fix smell code

--- backend/src/account/account.service.ts
+++ backend/src/account/account.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { UnExpectedServerException } from '../common/exceptions/un-expected.server.exception';
 import { UserRegisterInputDTO } from './dtos/user.register.input.dto';
 import { UserBadRequestException } from './exceptions/user.bad-request.exception';
 import { UserForbiddenException } from './exceptions/user.forbidden.exception';
