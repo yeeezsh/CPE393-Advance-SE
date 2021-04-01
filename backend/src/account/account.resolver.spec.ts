c1f5ced2326822653756a8aa5ade14b1551720c0
yee2542 <yee2542gtl@gmail.com>
2021-04-01 22:37:15 +0700

    add account resolver test

--- backend/src/account/account.resolver.spec.ts
+++ backend/src/account/account.resolver.spec.ts
@@ -1,13 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AccountResolver } from './account.resolver';
 import { AccountService } from './account.service';
-import { MOCK_USER_MODEL } from './tests/mock.user.model';
+import { UserRegisterInputDTO } from './dtos/user.register.input.dto';
+import { MOCK_USER_MODEL, MOCK_USER_VALUE } from './tests/mock.user.model';
+
+const MOCK_USER_INPUT_FOR_REGISTER = {
+  displayName: 'hello',
+  username: 'yee',
+  password: '123456',
+  email: 'y@e.com',
+} as UserRegisterInputDTO;
 
 describe('AccountResolver', () => {
   let provider: AccountResolver;
 
   beforeAll(async () => {
-    // const accServiceSpy = jest.spyOn(AccountService, '');
     const module: TestingModule = await Test.createTestingModule({
       providers: [AccountResolver, AccountService, MOCK_USER_MODEL],
     }).compile();
@@ -17,5 +24,11 @@ describe('AccountResolver', () => {
 
   it('should be defined', () => {
     expect(provider).toBeDefined();
+  });
+
+  it('should call AccountService.createAccount() when call createAccount endpoint', async () => {
+    const createService = jest.spyOn(MOCK_USER_VALUE, 'create');
+    await provider.createAccount(MOCK_USER_INPUT_FOR_REGISTER);
+    expect(createService).toBeCalled();
   });
 });
