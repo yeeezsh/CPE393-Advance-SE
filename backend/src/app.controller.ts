c711583145e8c69407fd216b832b3e4e5f23fc25
Thaweesak Saiwongse <57885091+Thaweesak-sai@users.noreply.github.com>
2021-04-08 20:43:17 +0700

    set route for login

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,10 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
-
+import { LocalAuthGuard } from './authhentication/local-auth.guard';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  async login(@Request() req: any) {
+    return req.user;
+  }
   @Get()
   getHello(): string {
     return this.appService.getHello();
