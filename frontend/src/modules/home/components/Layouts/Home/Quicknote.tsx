5c91be5690375d58d08ff011f101b00ffbc3c126
sirapitch limpanithiphat <hamzaakkk@hotmail.com>
2021-04-08 21:13:12 +0700

    add input field

--- frontend/src/modules/home/components/Layouts/Home/Quicknote.tsx
+++ frontend/src/modules/home/components/Layouts/Home/Quicknote.tsx
@@ -1,40 +1,55 @@
 import React, {useState} from 'react';
 
-const inputCard = {
+
+
+function Quicknote() {
+    return (
+        <div style = {styles.inputCard}>
+            <div style = {styles.inputHeader}> URL </div>
+            <input 
+                style = {styles.inputField}
+                type = "text"
+                placeholder = "Take a note"
+            />
+        </div>
+    );
+}
+
+const styles = {
+  inputCard: {
     width: '600px',
     margin: '20px auto',
     backgroundColor: '#FFFFFF',
     minHeight: '200px',
-    border: '1px light ',
+    border: '1px solid hsl(216, 8% ,88%)',
     borderRadius: '4px',
-
-  }
-  const inputHeader = {
+    
+  },
+  inputHeader: {
     fontFamily: 'Arial',
     fontSize:'15px',
-  }
-  const inputField = {
+    // backgroundColor: '#F5F6F7',
+    borderRadius: '2px 2px 0 0',
+    borderBottom: '1px solid #dddfe2 ',
+    margin: '0',
+    padding: '15px',
+
+
+  },
+  inputField: {
     height: '100px',
-    width: '100%',
-    padding: '10px auto',
+    width: '580px',
+    padding: '10px',
     border: 'none',
     fontFamily: 'Arial',
     fontSize:'20px',
-    
-
-  }
-function Quicknote() {
-    return (
-        <div className = "inputCard">
-            <div className = "inputHeader"> URL </div>
-            <input 
-                className = "inputField"
-                type = "text"
-            />
-        </div>
-    )
-}
-
-
+    outline: 'none',
+  
+  },
+  //   focus:{
+  //   outline: 'none',
+  
+  // }
+};
 
 export default Quicknote;
