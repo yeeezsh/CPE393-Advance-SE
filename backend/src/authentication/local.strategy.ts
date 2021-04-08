44bfc5466b7ad84cefc5c959ae74c900a9eef0fb
Thaweesak Saiwongse <57885091+Thaweesak-sai@users.noreply.github.com>
2021-04-09 04:27:51 +0700

    fix bad import

--- backend/src/authentication/local.strategy.ts
+++ backend/src/authentication/local.strategy.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport';
 import { UserLoginResponseDTO } from '../account/dtos/user.login.response.dto';
-import { UserInvalidCredentialException } from 'src/account/exceptions/user.invalid-credentials.exception';
+import { UserInvalidCredentialException } from '../account/exceptions/user.invalid-credentials.exception';
 import { UserLoginInputDTO } from '../account/dtos/user.login.input.dto';
 import { AuthenticationService } from './authentication.service';
 
