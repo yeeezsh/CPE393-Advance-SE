30fb71f11b6d9ced386d3ce0fa5df03065700ad7
Thaweesak Saiwongse <57885091+Thaweesak-sai@users.noreply.github.com>
2021-04-08 21:58:47 +0700

    Implement jwt

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,14 +1,26 @@
 import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
-import { LocalAuthGuard } from './authhentication/local-auth.guard';
+import { AuthenticationService } from './authentication/authentication.service';
+import { JwtAuthGuard } from './authentication/jwt-auth.guard';
+import { LocalAuthGuard } from './authentication/local-auth.guard';
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private authService: AuthenticationService,
+  ) {}
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
   async login(@Request() req: any) {
+    return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('main')
+  getMainPage(@Request() req: any) {
     return req.user;
   }
+
   @Get()
   getHello(): string {
     return this.appService.getHello();
--- backend/src/authentication/authentication.module.ts
+++ backend/src/authentication/authentication.module.ts
@@ -1,11 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AccountModule } from 'src/account/account.module';
 import { PassportModule } from '@nestjs/passport';
-import {AuthenticationService} from './authentication.service';
+import { AuthenticationService } from './authentication.service';
 import { LocalStrategy } from './local.strategy';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './constants';
+import { JwtStrategy } from './่jwt.strategy';
 @Module({
-  imports: [AccountModule, PassportModule],
-  providers: [AuthenticationService, LocalStrategy],
+  imports: [
+    AccountModule,
+    PassportModule,
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
+  providers: [AuthenticationService, LocalStrategy, JwtStrategy],
+  exports: [AuthenticationService, JwtModule],
   // controller: [AuthenticationController]
 })
 export class AuthenticationModule {}
