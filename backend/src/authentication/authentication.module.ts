2ca4aa5db32aded04453b01948872023f8719a23
yee2542 <yee2542gtl@gmail.com>
2021-04-23 02:31:20 +0700

    refactor

--- backend/src/authentication/authentication.module.ts
+++ backend/src/authentication/authentication.module.ts
@@ -1,20 +1,19 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { AccountModule } from '../account/account.module';
 import { AuthenticationResolver } from './authentication.resolver';
 import { AuthenticationService } from './authentication.service';
 import { JWT_CONSTANTS } from './constants';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
+
+const JwtProvider = JwtModule.register({
+  secret: JWT_CONSTANTS.secret,
+  signOptions: { expiresIn: '3600s' },
+});
 @Module({
-  imports: [
-    AccountModule,
-    PassportModule,
-    JwtModule.register({
-      secret: JWT_CONSTANTS.secret,
-      signOptions: { expiresIn: '3600s' },
-    }),
-  ],
-  providers: [AuthenticationService, AuthenticationResolver],
-  exports: [AuthenticationService],
+  imports: [forwardRef(() => AccountModule), PassportModule, JwtProvider],
+  providers: [AuthenticationService, AuthenticationResolver, JwtAuthGuard],
+  exports: [AuthenticationService, JwtAuthGuard, JwtProvider],
 })
 export class AuthenticationModule {}
--- backend/src/authentication/constants.ts
+++ backend/src/authentication/constants.ts
@@ -1,3 +1,3 @@
 export const JWT_CONSTANTS = {
-  secret: 'secretKey',
+  secret: '1234567890abcde',
 };
