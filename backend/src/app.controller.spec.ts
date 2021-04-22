76db919e9a77c3454e30c3598c3529d05a7ca974
yee2542 <yee2542gtl@gmail.com>
2021-04-23 01:08:59 +0700

    remove unused code

--- backend/src/app.controller.spec.ts
+++ backend/src/app.controller.spec.ts
@@ -1,15 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { AccountModule } from './account/account.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthenticationModule } from './authentication/authentication.module';
 
 describe('AppController', () => {
   let appController: AppController;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
-      imports: [AuthenticationModule, AccountModule],
       controllers: [AppController],
       providers: [AppService],
     }).compile();
@@ -20,10 +17,4 @@ describe('AppController', () => {
   it('should be defined', () => {
     expect(appController).toBeDefined();
   });
-
-  // describe('root', () => {
-  //   it('should return "Hello World!"', () => {
-  //     expect(appController.getHello()).toBe('Hello World!');
-  //   });
-  // });
 });
--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,29 +1,11 @@
-import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { AuthenticationService } from './authentication/authentication.service';
-import { JwtAuthGuard } from './authentication/guards/jwt-auth.guard';
-import { LocalAuthGuard } from './authentication/guards/local-auth.guard';
 @Controller()
 export class AppController {
-  constructor(
-    private readonly appService: AppService,
-    private readonly authService: AuthenticationService,
-  ) {}
+  constructor(private readonly appService: AppService) {}
 
-  @UseGuards(LocalAuthGuard)
-  @Post('auth/login')
-  async login(@Request() req: any) {
-    return this.authService.login(req.user);
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
   }
-
-  @UseGuards(JwtAuthGuard)
-  @Get('main')
-  getMainPage(@Request() req: any) {
-    return req.user;
-  }
-
-  // @Get()
-  // getHello(): string {
-  //   return this.appService.getHello();
-  // }
 }
