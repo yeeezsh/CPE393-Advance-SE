2cf25002111005723cea3cfc8a6194d7a87f3775
yee2542 <yee2542gtl@gmail.com>
2021-04-23 01:14:10 +0700

    remove unsued endpoint

--- backend/src/account/account.resolver.ts
+++ backend/src/account/account.resolver.ts
@@ -2,8 +2,6 @@ import { UseFilters } from '@nestjs/common';
 import { Args, Mutation, Resolver } from '@nestjs/graphql';
 import { AccountService } from './account.service';
 import { UserDTO } from './dtos/user.dto';
-import { UserLoginInputDTO } from './dtos/user.login.input.dto';
-import { UserLoginResponseDTO } from './dtos/user.login.response.dto';
 import { UserRegisterInputDTO } from './dtos/user.register.input.dto';
 import { UserResponseDTO } from './dtos/user.response.dto';
 import { UserMongoErrorFilterException } from './exceptions/filters/user.mongo-error.filter-exception';
@@ -19,12 +17,5 @@ export class AccountResolver {
   ): Promise<UserResponseDTO> {
     const user = await this.accountService.createAccount(userRegisterInputDTO);
     return user as UserResponseDTO;
-  }
-
-  async getByEmail(
-    @Args('UserLoginInputDTO') userLoginInputDTO: UserLoginInputDTO,
-  ): Promise<UserLoginResponseDTO> {
-    const user = await this.accountService.getByEmail(userLoginInputDTO.email);
-    return user as UserLoginResponseDTO;
   }
 }
