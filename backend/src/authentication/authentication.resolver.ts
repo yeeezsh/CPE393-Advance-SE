01edfd7d44fe680be8b89ff8b49a21f2037b264a
yee2542 <yee2542gtl@gmail.com>
2021-04-23 01:19:17 +0700

    add userLogin endpoint

---
+++ backend/src/authentication/authentication.resolver.ts
@@ -0,0 +1,21 @@
+import { Args, Context, Mutation, Resolver } from '@nestjs/graphql';
+import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
+import { AuthenticationService } from './authentication.service';
+import { UserLoginInputDTO } from './dtos/user.login.input.dto';
+import { UserLoginResponseDTO } from './dtos/user.login.response.dto';
+
+@Resolver(() => UserLoginResponseDTO)
+export class AuthenticationResolver {
+  constructor(private readonly authenticationService: AuthenticationService) {}
+
+  @Mutation(() => UserLoginResponseDTO)
+  async userLogin(
+    @Args('UserLogin') userLogin: UserLoginInputDTO,
+    @Context('req') req: Request,
+  ): Promise<UserLoginResponseDTO> {
+    const user = await this.authenticationService.login(userLogin);
+    req.res?.cookie('Authorization', user.token);
+    return { ...user, token: undefined };
+  }
+}
