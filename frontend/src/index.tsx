9bc1966a2749898e1428ea71fa2658883eb85f99
yee2542 <yee2542gtl@gmail.com>
2021-04-23 14:29:00 +0700

    implement store

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,16 +1,20 @@
 import { ApolloProvider } from "@apollo/client";
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import { apolloClient } from "./common/services/apollo.client";
+import { store } from "./common/store";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import Router from "./router";
 
 ReactDOM.render(
   <React.StrictMode>
-    <ApolloProvider client={apolloClient}>
-      <Router />
-    </ApolloProvider>
+    <Provider store={store}>
+      <ApolloProvider client={apolloClient}>
+        <Router />
+      </ApolloProvider>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
