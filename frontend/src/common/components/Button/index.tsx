aaec9ccc11b3b2d970b43a0b53239da2e3b5aa98
yee2542 <yee2542gtl@gmail.com>
2021-04-25 04:15:52 +0700

    allow types props

--- frontend/src/common/components/Button/index.tsx
+++ frontend/src/common/components/Button/index.tsx
@@ -6,11 +6,20 @@ export type ButtonProps = {
   rounded?: boolean;
   width?: number;
   onClick?: () => void;
+  type?:
+    | "text"
+    | "link"
+    | "ghost"
+    | "default"
+    | "primary"
+    | "dashed"
+    | undefined;
 };
 
 const Button: React.FC<ButtonProps> = (props) => {
   return (
     <AntdButton
+      type={props.type}
       style={{
         ...props.style,
         borderRadius: props?.rounded ? "1em" : "0%",
