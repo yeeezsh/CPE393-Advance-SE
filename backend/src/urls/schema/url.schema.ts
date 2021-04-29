62655b3b18bf29fc0cb0d041f0797195ecfd8b7f
yee2542 <yee2542gtl@gmail.com>
2021-04-29 22:33:45 +0700

    add url schema

---
+++ backend/src/urls/schema/url.schema.ts
@@ -0,0 +1,39 @@
+import { Prop, Schema } from '@nestjs/mongoose';
+import { Types } from 'mongoose';
+import { User } from '../../account/schema/user.schema';
+
+export type UrlDocument = Url & Document;
+
+@Schema()
+export class Url {
+  @Prop({
+    type: Types.ObjectId,
+    ref: User.name,
+    required: true,
+  })
+  owner: Types.ObjectId;
+
+  @Prop({
+    required: true,
+  })
+  original: string;
+
+  @Prop({
+    required: true,
+  })
+  doamin: string;
+
+  @Prop({
+    required: true,
+  })
+  subdomain: string;
+
+  @Prop()
+  note: string;
+
+  @Prop({ required: true, default: Date.now })
+  createAt: Date;
+
+  @Prop({ required: true, default: Date.now })
+  updateAt: Date;
+}
