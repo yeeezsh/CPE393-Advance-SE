ab714f0baf4fbfbcc629e4180402ab09f946e9a7
yee2542 <yee2542gtl@gmail.com>
2021-04-30 01:07:55 +0700

    fill url dependency

--- backend/src/urls/urls.service.spec.ts
+++ backend/src/urls/urls.service.spec.ts
@@ -1,4 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { MOCK_TAG_MODEL } from './tests/mock.tag.model';
+import { MOCK_URL_MODEL } from './tests/mock.url.model';
 import { UrlsService } from './urls.service';
 
 describe('UrlsService', () => {
@@ -6,7 +8,7 @@ describe('UrlsService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UrlsService],
+      providers: [MOCK_URL_MODEL, MOCK_TAG_MODEL, UrlsService],
     }).compile();
 
     service = module.get<UrlsService>(UrlsService);
