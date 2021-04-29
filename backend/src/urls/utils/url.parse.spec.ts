e27461c6a5226a3a64ce59b363a0b113578abd4b
yee2542 <yee2542gtl@gmail.com>
2021-04-29 23:52:46 +0700

    add url parser test

---
+++ backend/src/urls/utils/url.parse.spec.ts
@@ -0,0 +1,22 @@
+import urlParse from './url.parse';
+
+describe('Url parse util', () => {
+  it('Should parse with http:// prefix correctly', () => {
+    const sample = 'http://google.co.th';
+    const { domain, original } = urlParse(sample);
+    expect(domain).toBe('google.co.th');
+    expect(original).toBe(sample);
+  });
+
+  it('Should parse with subdomain correctly', () => {
+    const sample = 'https://docs.google.co.th';
+    const { domain } = urlParse(sample);
+    expect(domain).toBe('docs.google.co.th');
+  });
+
+  it('Should parse with subdomain & query string, param correctly', () => {
+    const sample = 'https://docs.google.co.th/hello?test=true';
+    const { domain } = urlParse(sample);
+    expect(domain).toBe('docs.google.co.th');
+  });
+});
