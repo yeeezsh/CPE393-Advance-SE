4c83f06b08f83f2739f5112107ceba4de8d22938
yee2542 <yee2542gtl@gmail.com>
2021-04-29 22:53:28 +0700

    register schema

--- backend/src/urls/urls.module.ts
+++ backend/src/urls/urls.module.ts
@@ -1,7 +1,19 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Tag, TagSchema } from './schema/tag.schema';
+import { Url, UrlSchema } from './schema/url.schema';
 import { UrlsService } from './urls.service';
 
 @Module({
-  providers: [UrlsService]
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Url.name,
+        schema: UrlSchema,
+      },
+      { name: Tag.name, schema: TagSchema },
+    ]),
+  ],
+  providers: [UrlsService],
 })
 export class UrlsModule {}
