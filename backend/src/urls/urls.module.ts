6c73561f2af06def3e23b5e51b0e1c66f91f70c5
yee2542 <yee2542gtl@gmail.com>
2021-05-01 04:25:32 +0700

    remove unused export

--- backend/src/urls/urls.module.ts
+++ backend/src/urls/urls.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Tag, TagSchema } from './schema/tag.schema';
 import { Url, UrlSchema } from './schema/url.schema';
+import { TagsResolver } from './tags.resolve';
 import { UrlsResolver } from './urls.resolver';
 import { UrlsService } from './urls.service';
 
@@ -15,7 +16,6 @@ import { UrlsService } from './urls.service';
       { name: Tag.name, schema: TagSchema },
     ]),
   ],
-  providers: [UrlsService, UrlsResolver],
-  exports: [UrlsService, UrlsResolver],
+  providers: [UrlsService, UrlsResolver, TagsResolver],
 })
 export class UrlsModule {}
