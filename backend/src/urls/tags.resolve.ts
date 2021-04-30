edd36e0d09fb611993a95e95e1c98419971a26a0
yee2542 <yee2542gtl@gmail.com>
2021-05-01 04:30:58 +0700

    complete tag resolver

--- backend/src/urls/tags.resolve.ts
+++ backend/src/urls/tags.resolve.ts
@@ -1,12 +1,24 @@
 import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { TagCreateInputDTO } from './dtos/input/tag-create.input.dto';
+import { TagEditInputDTO } from './dtos/input/tag-edit.input.dto';
 import { TagDTO } from './dtos/tag.dto';
+import { TagService } from './tag.service';
 
 @Resolver(() => TagDTO)
 export class TagsResolver {
+  constructor(private readonly tagService: TagService) {}
+
   @Mutation(() => TagDTO)
   async addTag(
-    @Args('')
-  ):Promise<TagDTO {
-    return;
+    @Args('TagCreateInputDTO') tagCreateInputDTO: TagCreateInputDTO,
+  ): Promise<TagDTO> {
+    return this.tagService.addTag(tagCreateInputDTO);
+  }
+
+  @Mutation(() => TagDTO)
+  async editTag(
+    @Args('TagEditInputDTO') tagEditInputDTO: TagEditInputDTO,
+  ): Promise<TagDTO> {
+    return this.tagService.editTag(tagEditInputDTO);
   }
 }
