a3ba9fa030a6f10e91db237b8b99aeddfdc73662
yee2542 <yee2542gtl@gmail.com>
2021-05-01 04:35:08 +0700

    fix bad import providrs

--- backend/src/urls/urls.module.ts
+++ backend/src/urls/urls.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Tag, TagSchema } from './schema/tag.schema';
 import { Url, UrlSchema } from './schema/url.schema';
+import { TagService } from './tag.service';
 import { TagsResolver } from './tags.resolve';
 import { UrlsResolver } from './urls.resolver';
 import { UrlsService } from './urls.service';
@@ -16,6 +17,6 @@ import { UrlsService } from './urls.service';
       { name: Tag.name, schema: TagSchema },
     ]),
   ],
-  providers: [UrlsService, UrlsResolver, TagsResolver],
+  providers: [UrlsService, TagService, UrlsResolver, TagsResolver],
 })
 export class UrlsModule {}
