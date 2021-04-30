2aee7faf14a5282fe073fb09fcb1ed1bea11687a
yee2542 <yee2542gtl@gmail.com>
2021-05-01 01:06:04 +0700

    complete url endpoint

--- backend/src/urls/urls.resolver.ts
+++ backend/src/urls/urls.resolver.ts
@@ -1,10 +1,24 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { UrlCreateInputDTO } from './dtos/input/url-create.input';
+import { UrlEditInputDTO } from './dtos/input/url-edit.input.dto';
 import { UrlDTO } from './dtos/url.dto';
+import { UrlsService } from './urls.service';
 
 @Resolver(() => UrlDTO)
 export class UrlsResolver {
-  @Query(() => UrlDTO)
-  async getUrls() {
-    return;
+  constructor(private readonly urlService: UrlsService) {}
+
+  @Mutation(() => UrlDTO)
+  async addUrls(
+    @Args('UrlCreateInputDTO') urlCreateInputDTO: UrlCreateInputDTO,
+  ): Promise<UrlDTO> {
+    return this.urlService.addUrl(urlCreateInputDTO);
+  }
+
+  @Mutation(() => UrlDTO)
+  async editUrls(
+    @Args('UrlEditInputDTO') urlEditInputDTO: UrlEditInputDTO,
+  ): Promise<UrlDTO> {
+    return this.urlService.editUrl(urlEditInputDTO);
   }
 }
