6079f0bddabf278de60c723a64f8a8b90a1c8234
yee2542 <yee2542gtl@gmail.com>
2021-05-01 04:16:02 +0700

    add tag service

---
+++ backend/src/urls/tag.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { TagType } from './@types/tag-type.type';
+import { TagEditInputDTO } from './dtos/input/tag-create.input.dto';
+import { TagCreateInputDTO } from './dtos/input/tag-edit.input.dto';
+import { TagDTO } from './dtos/tag.dto';
+import { Tag, TagDocument } from './schema/tag.schema';
+
+@Injectable()
+export class TagService {
+  constructor(@InjectModel(Tag.name) private tagModel: Model<TagDocument>) {}
+
+  async addTag(create: TagCreateInputDTO): Promise<TagDTO> {
+    const now = new Date();
+    const doc = await this.tagModel.create({
+      ...create,
+      createAt: now,
+      updateAt: now,
+    });
+    return doc as TagDTO;
+  }
+
+  async editTag(update: TagEditInputDTO): Promise<TagDTO> {
+    const now = new Date();
+    const updated = await this.tagModel.findByIdAndUpdate(
+      update._id,
+      {
+        ...update,
+        updateAt: now,
+        type: TagType.tag,
+      },
+      { new: true },
+    );
+
+    return updated as TagDTO;
+  }
+}
---
+++ backend/src/urls/tags.service.spec.ts
@@ -0,0 +1,48 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { Types } from 'mongoose';
+import { TagType } from './@types/tag-type.type';
+import { TagEditInputDTO } from './dtos/input/tag-create.input.dto';
+import { TagCreateInputDTO } from './dtos/input/tag-edit.input.dto';
+import { TagService } from './tag.service';
+import { MOCK_TAG_MODEL, MOCK_TAG_VALUE } from './tests/mock.tag.model';
+import { MOCK_URL_MODEL } from './tests/mock.url.model';
+
+describe('TagsService', () => {
+  let service: TagService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [MOCK_URL_MODEL, MOCK_TAG_MODEL, TagService],
+    }).compile();
+
+    service = module.get<TagService>(TagService);
+  });
+
+  it('Should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  it('Should able to call create when using addUrl', async () => {
+    const MOCK_URL_INPUT = {
+      owner: Types.ObjectId().toHexString(),
+      label: 'test lable',
+      type: TagType.tag,
+    } as TagCreateInputDTO;
+
+    await service.addTag(MOCK_URL_INPUT);
+    const model = jest.spyOn(MOCK_TAG_VALUE, 'create');
+    expect(model).toBeCalled();
+  });
+
+  it('Should able to call findByIdAndUpdate when using editUrl', async () => {
+    const MOCK_URL_INPUT = {
+      owner: Types.ObjectId().toHexString(),
+      label: 'test lable',
+      type: TagType.tag,
+    } as TagEditInputDTO;
+
+    await service.editTag(MOCK_URL_INPUT);
+    const model = jest.spyOn(MOCK_TAG_VALUE, 'findByIdAndUpdate');
+    expect(model).toBeCalled();
+  });
+});
