d1e00c48049bf265aedefb8006d02f9cc3f2888b
yee2542 <yee2542gtl@gmail.com>
2021-05-01 01:05:34 +0700

    complete url service

--- backend/src/urls/urls.service.ts
+++ backend/src/urls/urls.service.ts
@@ -1,31 +1,44 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model, Types } from 'mongoose';
+import { UrlCreateInputDTO } from './dtos/input/url-create.input';
+import { UrlEditInputDTO } from './dtos/input/url-edit.input.dto';
 import { UrlDTO } from './dtos/url.dto';
 import { UrlBadIdException } from './exceptions/url-bad-id.exceptions';
-import { Tag, TagDocument } from './schema/tag.schema';
 import { Url, UrlDocument } from './schema/url.schema';
+import urlParse from './utils/url.parse';
 
 @Injectable()
 export class UrlsService {
-  constructor(
-    @InjectModel(Url.name) private urlModel: Model<UrlDocument>,
-    @InjectModel(Tag.name) private tagModel: Model<TagDocument>,
-  ) {}
+  constructor(@InjectModel(Url.name) private urlModel: Model<UrlDocument>) {}
 
-  async addUrl(url: UrlDTO): Promise<UrlDocument> {
+  async addUrl(create: UrlCreateInputDTO): Promise<UrlDTO> {
     const now = new Date();
+    const { original, domain } = urlParse(create.original);
     const doc = await this.urlModel.create({
-      ...url,
+      ...create,
+      original,
+      domain,
       createAt: now,
       updateAt: now,
     });
-    return doc;
+    return doc as UrlDTO;
   }
 
-  async editUrl(id: Types.ObjectId, update: UrlDTO): Promise<UrlDocument> {
-    const updated = await this.urlModel.findByIdAndUpdate(id, update);
+  async editUrl(update: UrlEditInputDTO): Promise<UrlDTO> {
+    const now = new Date();
+    const { original, domain } = urlParse(update.original);
+    const updated = await this.urlModel.findByIdAndUpdate(
+      update._id,
+      {
+        ...update,
+        domain: domain ?? '',
+        original,
+        updateAt: now,
+      },
+      { new: true },
+    );
     if (!updated) throw new UrlBadIdException();
-    return updated;
+    return updated as UrlDTO;
   }
 }
