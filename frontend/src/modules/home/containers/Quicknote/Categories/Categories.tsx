cfee23583dc0c41e1385d8bfa2c80bf4a23f12e1
Thaweesak Saiwongse <Thaweesak_sai@hotmail.com>
2021-05-03 05:07:45 +0700

    Fix bad import

--- frontend/src/modules/home/containers/Quicknote/Categories/Categories.tsx
+++ frontend/src/modules/home/containers/Quicknote/Categories/Categories.tsx
@@ -1,42 +1,44 @@
-import React, { useState } from 'react';
-import { Tag } from 'antd';
+import React, { useState } from "react";
+import { Tag } from "antd";
 const { CheckableTag } = Tag;
 
-const tags = ['Movies', 'Books', 'Music', 'Sports'];
-
+const tags = ["Movies", "Books", "Music", "Sports"];
 
 const Categories: React.FC = () => {
+  const [tags, setTags] = useState<
+    {
+      label: string;
+      checked: boolean;
+    }[]
+  >([]);
 
-    const [tags, setTags] = useState<{
-        label: string,
-        checked: boolean,
-    }>([]);
+  const onCheck = (label: string) => {
+    return (checked: boolean) => {
+      setTags((s) =>
+        s.map((el: { label: string; checked: any }) => {
+          if (el.label === label) return { ...el, check: !el.checked };
+          return el;
+        })
+      );
+    };
+  };
 
-    const onCheck = (label: string) => {
-        return (checked: boolean) => {
-            setTags(s => s.map((el: { label: string; checked: any; }) => {
-                if (el.label === label) return { ...el, check: !el.checked }
-                return el;
-            }))
-        }
-    }
-
-    return (
-        <>
-            <div>
-                <strong style={{ marginRight: 8, marginLeft: 8 }}>Categories:</strong>
-                {tags.map(tag => (
-                    <CheckableTag
-                        key={tag}
-                        checked={tag.checked}
-                        onChange={onCheck(tag)}
-                    >
-                        {tag}
-                    </CheckableTag>
-                ))}
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div>
+        <strong style={{ marginRight: 8, marginLeft: 8 }}>Categories:</strong>
+        {tags.map((tag) => (
+          <CheckableTag
+            key={tag.label || ""}
+            checked={tag.checked}
+            onChange={onCheck(tag.label)}
+          >
+            {tag}
+          </CheckableTag>
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default Categories;
