a99e24dd5af62e88df6b5f0830b7d0bacfeab3aa
yee2542 <yee2542gtl@gmail.com>
2021-05-03 06:51:31 +0700

    add account badge test

---
+++ frontend/src/modules/home/components/AccountBadge/index.spec.tsx
@@ -0,0 +1,25 @@
+import { mount, render, shallow } from "enzyme";
+import React from "react";
+import AccountBadge, { AccountBadgeProps } from ".";
+import Overlay from "./overlay";
+import { AccountBadgeStyle } from "./styled";
+
+const MOCK_BADGE_PROPS: AccountBadgeProps = {
+  username: "john",
+  email: "john@doe.com",
+  displayname: "john doe",
+};
+
+describe("Components/AccountBadge", () => {
+  it("Should show overlay when clicked", () => {
+    const wrap = shallow(<AccountBadge {...MOCK_BADGE_PROPS} />);
+    const badge = wrap.find(AccountBadgeStyle);
+    badge.simulate("click");
+    expect(wrap.find(Overlay).exists()).toBe(true);
+  });
+
+  it("Should not show overlay when clicked", () => {
+    const wrap = shallow(<AccountBadge {...MOCK_BADGE_PROPS} />);
+    expect(wrap.find(Overlay).exists()).toBe(false);
+  });
+});
