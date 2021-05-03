c14b94e4cb42d459166d8748f6cf585256ad674a
Thaweesak Saiwongse <Thaweesak_sai@hotmail.com>
2021-05-03 19:39:50 +0700

    center the card

--- frontend/src/modules/account/pages/signup/index.tsx
+++ frontend/src/modules/account/pages/signup/index.tsx
@@ -24,115 +24,117 @@ const SignUp: React.FC = () => {
   });
 
   const onFinish = (values: any) => {
-const createAccountData = {
-  displayName : values.username,
-  username : values.username,
-  email: values.email,
-  password: values.password,
-};
+    const createAccountData = {
+      displayName: values.username,
+      username: values.username,
+      email: values.email,
+      password: values.password,
+    };
 
     console.log("Success:", createAccountData);
     createAccountMutation({ variables: { user: createAccountData } });
-    console.log("helloworld",data?.createAccount);
-    console.log("ERRRO SHOULD BE HERE",error?.message);
+    console.log("helloworld", data?.createAccount);
+    console.log("ERRRO SHOULD BE HERE", error?.message);
   };
 
   return (
-    <div className="site-card-border-less-wrapper" style={componentLayout}>
-      <Card bordered={true} style={{ width: 450, textAlign: "center" }}>
-        <Title level={4} style={componentLayout}>
-          Sign up for your account
-        </Title>
-        <Row style={{ marginBottom: 16 }}></Row>
-        <Form
-          form={form}
-          name="register"
-          layout="horizontal"
-          scrollToFirstError
-          onFinish={onFinish}
-        >
-          <Form.Item
-            name="email"
-            rules={[
-              {
-                type: "email",
-                message: "The input is not a valid E-mail",
-              },
-              {
-                required: true,
-                message: "Please input your Email",
-              },
-            ]}
+    <Row justify="center" align="middle" style={{ minHeight: "100vh" }}>
+      <div className="site-card-border-less-wrapper" style={componentLayout}>
+        <Card bordered={true} style={{ width: 450, textAlign: "center" }}>
+          <Title level={4} style={componentLayout}>
+            Sign up for your account
+          </Title>
+          <Row style={{ marginBottom: 16 }}></Row>
+          <Form
+            form={form}
+            name="register"
+            layout="horizontal"
+            scrollToFirstError
+            onFinish={onFinish}
           >
-            <Input
-              prefix={<MailOutlined className="site-form-item-icon" />}
-              placeholder="Enter email address"
-            />
-          </Form.Item>
-          <Form.Item
-            name="username"
-            rules={[
-              {
-                required: true,
-                message: "Please input your Username",
-              },
-            ]}
-          >
-            <Input prefix={<UserOutlined />} placeholder="Enter Username" />
-          </Form.Item>
-
-          <Form.Item
-            name="password"
-            rules={[
-              {
-                required: true,
-                message: "Please input your Password",
-              },
-            ]}
-            hasFeedback
-          >
-            <Input.Password
-              prefix={<LockOutlined />}
-              placeholder="Enter Password"
-            />
-          </Form.Item>
-
-          <Form.Item
-            name="confirmPassword"
-            dependencies={["password"]}
-            hasFeedback
-            rules={[
-              {
-                required: true,
-                message: "Please input your Username",
-              },
-              ({ getFieldValue }) => ({
-                validator(_, value) {
-                  if (!value || getFieldValue("password") === value) {
-                    return Promise.resolve();
-                  }
-                  return Promise.reject(
-                    new Error(
-                      "The two passwords that you entered do not match!"
-                    )
-                  );
+            <Form.Item
+              name="email"
+              rules={[
+                {
+                  type: "email",
+                  message: "The input is not a valid E-mail",
                 },
-              }),
-            ]}
-          >
-            <Input.Password placeholder="Re-enter password" />
-          </Form.Item>
-
-          <Space align="center">
-            <Form.Item {...tailLayout}>
-              <Button type="primary" htmlType="submit">
-                Register
-              </Button>
+                {
+                  required: true,
+                  message: "Please input your Email",
+                },
+              ]}
+            >
+              <Input
+                prefix={<MailOutlined className="site-form-item-icon" />}
+                placeholder="Enter email address"
+              />
             </Form.Item>
-          </Space>
-        </Form>
-      </Card>
-    </div>
+            <Form.Item
+              name="username"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your Username",
+                },
+              ]}
+            >
+              <Input prefix={<UserOutlined />} placeholder="Enter Username" />
+            </Form.Item>
+
+            <Form.Item
+              name="password"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your Password",
+                },
+              ]}
+              hasFeedback
+            >
+              <Input.Password
+                prefix={<LockOutlined />}
+                placeholder="Enter Password"
+              />
+            </Form.Item>
+
+            <Form.Item
+              name="confirmPassword"
+              dependencies={["password"]}
+              hasFeedback
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your Username",
+                },
+                ({ getFieldValue }) => ({
+                  validator(_, value) {
+                    if (!value || getFieldValue("password") === value) {
+                      return Promise.resolve();
+                    }
+                    return Promise.reject(
+                      new Error(
+                        "The two passwords that you entered do not match!"
+                      )
+                    );
+                  },
+                }),
+              ]}
+            >
+              <Input.Password placeholder="Re-enter password" />
+            </Form.Item>
+
+            <Space align="center">
+              <Form.Item {...tailLayout}>
+                <Button type="primary" htmlType="submit">
+                  Register
+                </Button>
+              </Form.Item>
+            </Space>
+          </Form>
+        </Card>
+      </div>
+    </Row>
   );
 };
 
