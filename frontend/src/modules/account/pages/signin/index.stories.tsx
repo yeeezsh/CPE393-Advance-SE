cc74404c5b652626002390ef1c62e9270025539f
Thaweesak Saiwongse <Thaweesak_sai@hotmail.com>
2021-05-03 23:01:47 +0700

    fix storybook signin

--- frontend/src/modules/account/pages/signin/index.stories.tsx
+++ frontend/src/modules/account/pages/signin/index.stories.tsx
@@ -1,16 +1,24 @@
 import React from "react";
-import "./antd/dist/antd.css";
+import "antd/dist/antd.css";
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
 import { Story, Meta } from "@storybook/react/types-6-0";
 
 import SignInPage from ".";
+import { ApolloProvider } from "@apollo/client/react/context/ApolloProvider";
+import { apolloClient } from "../../../../common/services/apollo.client";
 
 export default {
   title: "Account/Pages/SignIn",
   component: SignInPage,
 } as Meta;
 
-const Template: Story = () => <SignInPage />;
+const Template: Story = () => {
+  return (
+    <ApolloProvider client={apolloClient}>
+      <SignInPage />{" "}
+    </ApolloProvider>
+  );
+};
 
 export const LoggedIn = Template.bind({});
 // LoggedIn.args = {
