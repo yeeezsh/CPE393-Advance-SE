a02c9b9e07a788fd11afce73f2bb0fe33824ab66
yee2542 <yee2542gtl@gmail.com>
2021-05-04 22:47:19 +0700

    refactor

--- frontend/src/modules/account/pages/signup/ErrorBadge.spec.tsx
+++ frontend/src/modules/account/pages/signup/ErrorBadge.spec.tsx
@@ -1,10 +1,17 @@
 import { shallow } from "enzyme";
-import ErrorBadge from "./ErrorBadge";
+import ErrorBadge, { DuplicationAlert, UnexptecErrorAlert } from "./ErrorBadge";
 
 describe("Pages/SignUp/ErrorBadge", () => {
-  it("Should render component when statusError=400,500", () => {
-    const wrap = shallow(<ErrorBadge statusError={400} />).text();
-    expect(wrap).not.toBe("");
+  it("Should render DuplicationAlert component when statusError=400", () => {
+    const wrap = shallow(<ErrorBadge statusError={400} />);
+    expect(wrap.find(DuplicationAlert).exists()).toBe(true);
+    expect(wrap.find(UnexptecErrorAlert).exists()).toBe(false);
+  });
+
+  it("Should render UnexptecErrorAlert component when statusError=500", () => {
+    const wrap = shallow(<ErrorBadge statusError={500} />);
+    console.log(wrap);
+    expect(wrap.find(UnexptecErrorAlert).exists()).toBe(true);
   });
 
   it("Should not render component when statusError=400,500", () => {
--- frontend/src/modules/account/pages/signup/ErrorBadge.tsx
+++ frontend/src/modules/account/pages/signup/ErrorBadge.tsx
@@ -2,28 +2,30 @@ import { SmileOutlined } from "@ant-design/icons";
 import { Alert } from "antd";
 import React from "react";
 
+export const DuplicationAlert: React.FC = () => (
+  <Alert
+    icon={<SmileOutlined />}
+    message="Error"
+    description="Something went wrong! Please try again later"
+    type="error"
+    showIcon
+  />
+);
+export const UnexptecErrorAlert: React.FC = () => (
+  <Alert
+    icon={<SmileOutlined />}
+    message="Error"
+    description="Something went wrong! Please try again later"
+    type="error"
+    showIcon
+  />
+);
 const ErrorBadge: React.FC<{ statusError: number }> = (props) => {
   const { statusError } = props;
   return (
     <>
-      {statusError === 400 && (
-        <Alert
-          icon={<SmileOutlined />}
-          message="Error"
-          description="The Email or Username is used!"
-          type="error"
-          showIcon
-        />
-      )}
-      {statusError === 500 && (
-        <Alert
-          icon={<SmileOutlined />}
-          message="Error"
-          description="Something went wrong! Please try again later"
-          type="error"
-          showIcon
-        />
-      )}
+      {statusError === 400 && <DuplicationAlert />}
+      {statusError === 500 && <UnexptecErrorAlert />}
     </>
   );
 };
