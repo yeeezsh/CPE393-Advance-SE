6435bb643976a0e9131974b1198d73e528a6e0e5
Thaweesak Saiwongse <Thaweesak_sai@hotmail.com>
2021-05-04 22:54:45 +0700

    remove unused

--- frontend/src/modules/account/pages/signup/index.spec.tsx
+++ frontend/src/modules/account/pages/signup/index.spec.tsx
@@ -5,54 +5,6 @@ import * as generateTypes from "../../../../common/services/generate/generate-ty
 import { CreateAccountDocument } from "../../../../common/services/generate/generate-types";
 
 describe("Page/Signup", () => {
-  const mocksError = [
-    {
-      request: {
-        query: CreateAccountDocument,
-        variables: {
-          displayName: "blablabla",
-          username: "blablabla",
-          email: "note@mail.com",
-          password: "123456",
-        },
-      },
-      result: { errors: [new GraphQLError("Duplicated keys: email,username")] },
-    },
-  ];
-
-  //   it("Should render error when graphql return error", async () => {
-  //     const { findByText, getByText } = render(
-  //       <MockedProvider mocks={mocksError} addTypename={false}>
-  //         <AccountSignUpPage />
-  //       </MockedProvider>
-  //     );
-  //     // console.log(wrapper.debug());
-  // // expect(getByText('The Email or Username is used!')).toBeInTheDocument();
-  // const errorAlert = await findByText("noError");
-  // expect(errorAlert).toBeInTheDocument();
-  // console.log(wrapper.html());
-  // expect(wrapper.contains(<div className="errorMessage400"></div>)).toEqual(
-  //   true
-  // );
-  // expect(wrapper.contains(<div className="normalDiv"></div>)).toEqual(false);
-  // expect(
-  //   wrapper.contains(
-  //     <Alert
-  //       icon={<SmileOutlined />}
-  //       message="Error"
-  //       description="The Email or Username is used!"
-  //       type="error"
-  //       showIcon
-  //     />
-  //   )
-  // ).toEqual(true);
-  // });
-
-  // it("should match snapshot", () => {
-  //   const wrap = renderer.create(<AccountSignUpPage />).toJSON();
-  //   expect(wrap).toMatchSnapshot();
-  // });
-
   it("Should render successfully", () => {
     const mockFn: any = () => {};
     const apollo: any = {};
