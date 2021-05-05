3082c4dabb55fa739b8413657b5c197b13cdeb27
yee2542 <yee2542gtl@gmail.com>
2021-05-06 06:18:41 +0700

    fix outside click detect

--- frontend/src/modules/home/components/AccountBadge/index.tsx
+++ frontend/src/modules/home/components/AccountBadge/index.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import CharacterBadge from "../../../../common/components/CharacterBadge";
+import useOnClickOutside from "../../../../common/hooks/useOnClickOutside";
 import AccountInfo from "./account.info";
 import Overlay, { OverlayProps } from "./overlay";
 import { AccountBadgeStyle, UsernameBadgeStyle } from "./styled";
@@ -22,9 +23,13 @@ const AccountBadge: React.FC<AccountBadgeProps> = (props) => {
   const firstCharacter = props.username.slice(0, 1).toLocaleUpperCase();
   const [overlay, setOverlay] = useState<boolean>(props?.overlay || false);
 
+  const ref = useRef(null);
+  useOnClickOutside(ref, () => setOverlay((o) => !o));
+
   return (
     <>
       <AccountBadgeStyle
+        ref={ref}
         style={props?.style}
         onClick={() => setOverlay((o) => !o)}
       >
