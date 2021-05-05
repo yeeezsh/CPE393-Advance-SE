550b63df536f1c07274604de2aae7c6ac3be386d
yee2542 <yee2542gtl@gmail.com>
2021-05-06 04:59:34 +0700

    fix import

--- backend/src/search/search.service.spec.ts
+++ backend/src/search/search.service.spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { MOCK_BOOKMARK_MODEL } from '../bookmark/tests/mock.bookmark.model';
 import { MOCK_TAG_MODEL } from '../bookmark/tests/mock.tag.model';
-import { MOCK_URL_MODEL } from '../bookmark/tests/mock.url.model';
 import { SearchService } from './search.service';
 
 describe('SearchService', () => {
@@ -8,7 +8,7 @@ describe('SearchService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [MOCK_URL_MODEL, MOCK_TAG_MODEL, SearchService],
+      providers: [MOCK_BOOKMARK_MODEL, MOCK_TAG_MODEL, SearchService],
     }).compile();
 
     service = module.get<SearchService>(SearchService);
