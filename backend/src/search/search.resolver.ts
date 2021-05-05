00a5af026c3f8a4c5f3cde95406ba1099defe82e
yee2542 <yee2542gtl@gmail.com>
2021-05-06 04:08:26 +0700

    wip search resolver

--- backend/src/search/search.resolver.ts
+++ backend/src/search/search.resolver.ts
@@ -1,4 +1,15 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Query, Resolver } from '@nestjs/graphql';
+import { SearchTextInputDTO } from './dtos/input/search-text.input';
+import { SearchDTO } from './dtos/search.dto';
 
-@Resolver()
-export class SearchResolver {}
+@Resolver(() => SearchDTO)
+export class SearchResolver {
+  @Query(() => SearchDTO)
+  async allTextSearchBookmark(
+    @Args(SearchTextInputDTO.name) search: SearchTextInputDTO,
+  ): Promise<SearchDTO> {
+    return {
+      results: [],
+    };
+  }
+}
