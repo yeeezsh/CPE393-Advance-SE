e81250bdce62308fcd3b21c72fe69359cb77b409
yee2542 <yee2542gtl@gmail.com>
2021-05-06 04:17:50 +0700

    fix bad test

--- backend/src/search/search.service.spec.ts
+++ backend/src/search/search.service.spec.ts
@@ -1,4 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { MOCK_TAG_MODEL } from '../bookmark/tests/mock.tag.model';
+import { MOCK_URL_MODEL } from '../bookmark/tests/mock.url.model';
 import { SearchService } from './search.service';
 
 describe('SearchService', () => {
@@ -6,7 +8,7 @@ describe('SearchService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [SearchService],
+      providers: [MOCK_URL_MODEL, MOCK_TAG_MODEL, SearchService],
     }).compile();
 
     service = module.get<SearchService>(SearchService);
