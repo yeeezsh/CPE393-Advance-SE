3fa95fd5063b42664985f3201deb0bdee4539140
Thaweesak Saiwongse <Thaweesak_sai@hotmail.com>
2021-05-06 04:39:58 +0700

    implement resolver

--- backend/src/bookmark/tags.resolver.ts
+++ backend/src/bookmark/tags.resolver.ts
@@ -1,6 +1,9 @@
 import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { BookmarkDTO } from './dtos/bookmark.dto';
 import { TagCreateInputDTO } from './dtos/input/tag-create.input.dto';
+import { TagDeleteInputDTO } from './dtos/input/tag-delete.input.dto';
 import { TagEditInputDTO } from './dtos/input/tag-edit.input.dto';
+import { TagSetArchiveInputDTO } from './dtos/input/tag-setArchive.input.dto';
 import { TagDTO } from './dtos/tag.dto';
 import { TagService } from './tag.service';
 
@@ -20,5 +23,15 @@ export class TagsResolver {
     @Args('TagEditInputDTO') tagEditInputDTO: TagEditInputDTO,
   ): Promise<TagDTO> {
     return this.tagService.editTag(tagEditInputDTO);
+  }
+
+  @Mutation(()=>TagDTO)
+  async deleteTag(@Args('TagDeleteInputDTO') tagDeleteInputDTO: TagDeleteInputDTO):Promise<void>{
+    return this.tagService.deleteTag(tagDeleteInputDTO);
+  }
+
+  @Mutation(()=>TagDTO)
+  async setArchiveTag(@Args('TagSetArchiveDTO') tagSetArchiveInputDTO: TagSetArchiveInputDTO): Promise<BookmarkDTO>{
+    return this.tagService.archiveTag(tagSetArchiveInputDTO);
   }
 }
