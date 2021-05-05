fa66ce071b989b06115a76823d02a3cf0ad92fee
yee2542 <yee2542gtl@gmail.com>
2021-05-06 03:41:16 +0700

    wip search service

--- backend/src/search/search.service.ts
+++ backend/src/search/search.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Bookmark, UrlDocument } from '../bookmark/schema/bookmark.schema';
+import { Tag, TagDocument } from '../bookmark/schema/tag.schema';
 
 @Injectable()
-export class SearchService {}
+export class SearchService {
+  constructor(
+    @InjectModel(Bookmark.name) private bookmarkModel: Model<UrlDocument>,
+    @InjectModel(Tag.name) private tagModel: Model<TagDocument>,
+  ) {}
+}
