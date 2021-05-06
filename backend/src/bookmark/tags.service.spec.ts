e897665977b8eebf8dc875face583f33546f82f5
Thaweesak Saiwongse <Thaweesak_sai@hotmail.com>
2021-05-07 04:04:02 +0700

    add test

--- backend/src/bookmark/tags.service.spec.ts
+++ backend/src/bookmark/tags.service.spec.ts
@@ -1,15 +1,33 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { Types } from 'mongoose';
+import { SystemTagType } from './@types/systemTag-type.type';
 import { TagType } from './@types/tag-type.type';
 import { BookmarkService } from './bookmark.service';
 import { TagCreateInputDTO } from './dtos/input/tag-create.input.dto';
 import { TagEditInputDTO } from './dtos/input/tag-edit.input.dto';
+import { TagSetArchiveInputDTO } from './dtos/input/tag-setArchive.input.dto';
 import { TagService } from './tag.service';
-import { MOCK_BOOKMARK_MODEL } from './tests/mock.bookmark.model';
+import {
+  MOCK_BOOKMARK_DOCUMENT,
+  MOCK_BOOKMARK_MODEL,
+} from './tests/mock.bookmark.model';
 import { MOCK_TAG_MODEL, MOCK_TAG_VALUE } from './tests/mock.tag.model';
 
 describe('Tags Service', () => {
   let service: TagService;
+
+  const MOCK_BOOKMARK_ID = Types.ObjectId().toHexString();
+  const MOCK_OWNER_ID = Types.ObjectId().toHexString();
+
+  const MOCK_BOOKMARK_SERVICE = {
+    getABookmark: () => ({
+      ...MOCK_BOOKMARK_DOCUMENT,
+      tags: ['tagA', 'tagB'],
+      _id: MOCK_BOOKMARK_ID,
+      _owner: MOCK_OWNER_ID,
+    }),
+    editBookmark: (a: any) => a,
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -19,7 +37,10 @@ describe('Tags Service', () => {
         TagService,
         BookmarkService,
       ],
-    }).compile();
+    })
+      .overrideProvider(BookmarkService)
+      .useValue(MOCK_BOOKMARK_SERVICE)
+      .compile();
 
     service = module.get<TagService>(TagService);
   });
@@ -52,10 +73,43 @@ describe('Tags Service', () => {
     expect(model).toBeCalled();
   });
 
-  // it('Should able to return updatedBookmarkDTO when call setArchiveTag', async () => {
+  it('Should able to addTag when call setArchiveTag', async () => {
+    const MOCK_TAG_SET_ARCHIVE_INPUT = {
+      bookmarkId: Types.ObjectId().toHexString(),
+    } as TagSetArchiveInputDTO;
+    const now = new Date();
+
+    const MOCK_RETURN_BOOKMARK_DOC = {
+      _id: MOCK_BOOKMARK_ID,
+      owner: MOCK_OWNER_ID,
+      original: 'https://docs.google.com',
+      domain: 'docs.google.com',
+      note: '',
+      tags: ['tagA', 'tagB', SystemTagType.archive],
+      createAt: now,
+      updateAt: now,
+    };
+    const returnVal = await service.setArchiveTag(MOCK_TAG_SET_ARCHIVE_INPUT);
+    expect(returnVal.tags).toEqual(MOCK_RETURN_BOOKMARK_DOC.tags);
+  });
+
+  // it('Should able to return setArchiveTag when call setArchiveTag', async () => {
   //   const MOCK_TAG_SET_ARCHIVE_INPUT = {
   //     bookmarkId: Types.ObjectId().toHexString(),
   //   } as TagSetArchiveInputDTO;
-  //   await service.setArchiveTag(MOCK_TAG_SET_ARCHIVE_INPUT);
+  //   const now = new Date();
+
+  //   const MOCK_RETURN_BOOKMARK_DOC = {
+  //     _id: MOCK_BOOKMARK_ID,
+  //     owner: MOCK_OWNER_ID,
+  //     original: 'https://docs.google.com',
+  //     domain: 'docs.google.com',
+  //     note: '',
+  //     tags: ['tagA', 'tagB', SystemTagType.archive],
+  //     createAt: now,
+  //     updateAt: now,
+  //   };
+  //   const returnVal = await service.setArchiveTag(MOCK_TAG_SET_ARCHIVE_INPUT);
+  //   expect(returnVal.tags).toEqual(MOCK_RETURN_BOOKMARK_DOC.tags);
   // });
 });
