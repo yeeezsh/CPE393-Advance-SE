ebfdc2c4b0abae35d0e6d2af6c90e1c870b4c943
Thaweesak Saiwongse <Thaweesak_sai@hotmail.com>
2021-05-07 03:15:25 +0700

    add unit test

--- backend/src/bookmark/bookmark.service.spec.ts
+++ backend/src/bookmark/bookmark.service.spec.ts
@@ -1,13 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { Types } from 'mongoose';
+import { BookmarkService } from './bookmark.service';
 import { BookmarkCreateInputDTO } from './dtos/input/bookmark-create.input';
 import { BookmarkEditInputDTO } from './dtos/input/bookmark-edit.input.dto';
-import { MOCK_TAG_MODEL } from './tests/mock.tag.model';
 import {
   MOCK_BOOKMARK_MODEL,
-  MOCK_BOOKMARK_VALUE,
+  MOCK_BOOKMARK_VALUE
 } from './tests/mock.bookmark.model';
-import { BookmarkService } from './bookmark.service';
+import { MOCK_TAG_MODEL } from './tests/mock.tag.model';
 
 describe('Bookmark Service', () => {
   let service: BookmarkService;
@@ -61,5 +61,12 @@ describe('Bookmark Service', () => {
     expect(jest.spyOn(MOCK_BOOKMARK_VALUE, 'skip')).toBeCalled();
     expect(jest.spyOn(MOCK_BOOKMARK_VALUE, 'limit')).toBeCalled();
     expect(jest.spyOn(MOCK_BOOKMARK_VALUE, 'lean')).toBeCalled();
+  });
+
+  it('Should call findById when using getABookMark', async () => {
+    const MOCK_BOOKMARK_ID = Types.ObjectId().toHexString();
+    await service.getABookmark(MOCK_BOOKMARK_ID);
+    const findById = jest.spyOn(MOCK_BOOKMARK_VALUE, 'findById');
+    expect(findById).toBeCalled();
   });
 });
--- backend/src/bookmark/tags.service.spec.ts
+++ backend/src/bookmark/tags.service.spec.ts
@@ -1,18 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { Types } from 'mongoose';
 import { TagType } from './@types/tag-type.type';
+import { BookmarkService } from './bookmark.service';
 import { TagCreateInputDTO } from './dtos/input/tag-create.input.dto';
 import { TagEditInputDTO } from './dtos/input/tag-edit.input.dto';
 import { TagService } from './tag.service';
-import { MOCK_TAG_MODEL, MOCK_TAG_VALUE } from './tests/mock.tag.model';
 import { MOCK_BOOKMARK_MODEL } from './tests/mock.bookmark.model';
+import { MOCK_TAG_MODEL, MOCK_TAG_VALUE } from './tests/mock.tag.model';
 
 describe('Tags Service', () => {
   let service: TagService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [MOCK_BOOKMARK_MODEL, MOCK_TAG_MODEL, TagService],
+      providers: [
+        MOCK_BOOKMARK_MODEL,
+        MOCK_TAG_MODEL,
+        TagService,
+        BookmarkService,
+      ],
     }).compile();
 
     service = module.get<TagService>(TagService);
@@ -45,4 +51,11 @@ describe('Tags Service', () => {
     const model = jest.spyOn(MOCK_TAG_VALUE, 'findByIdAndUpdate');
     expect(model).toBeCalled();
   });
+
+  // it('Should able to return updatedBookmarkDTO when call setArchiveTag', async () => {
+  //   const MOCK_TAG_SET_ARCHIVE_INPUT = {
+  //     bookmarkId: Types.ObjectId().toHexString(),
+  //   } as TagSetArchiveInputDTO;
+  //   await service.setArchiveTag(MOCK_TAG_SET_ARCHIVE_INPUT);
+  // });
 });
