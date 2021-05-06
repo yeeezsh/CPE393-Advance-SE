b1113ea40d1d8289409fb9e2e2db0c0686f1533f
Thaweesak Saiwongse <Thaweesak_sai@hotmail.com>
2021-05-07 04:24:41 +0700

    fix dto

--- backend/src/bookmark/dtos/input/bookmark-get.input.dto.ts
+++ backend/src/bookmark/dtos/input/bookmark-get.input.dto.ts
@@ -1,7 +1,7 @@
 import { Field, InputType } from '@nestjs/graphql';
 
 @InputType()
-export class TagDeleteInputDTO {
+export class BookmarkGetInputDTO {
   @Field()
   bookmarkId: string;
 }
--- backend/src/bookmark/dtos/input/tag-setArchive.input.dto.ts
+++
@@ -1,8 +0,0 @@
-import { Field, InputType, registerEnumType } from '@nestjs/graphql';
-import { TagType } from '../../@types/tag-type.type';
-
-@InputType()
-export class TagSetArchiveInputDTO {
-  @Field()
-  bookmarkId: string;
-}
