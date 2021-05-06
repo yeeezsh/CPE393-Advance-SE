97814de9e16234d09f836f993b9896bce2983abc
yee2542 <yee2542gtl@gmail.com>
2021-05-06 23:30:48 +0700

    add all text search

--- backend/src/search/search.service.ts
+++ backend/src/search/search.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { Model, Types } from 'mongoose';
 import { Bookmark, BookmarkDocument } from '../bookmark/schema/bookmark.schema';
 import { Tag, TagDocument } from '../bookmark/schema/tag.schema';
 import { SearchInputDTO } from './dtos/input/search.input';
@@ -14,6 +14,44 @@ export class SearchService {
   ) {}
 
   async allTextSearch(search: SearchInputDTO): Promise<SearchDTO> {
-    return { results: [] };
+    const results = await this.bookmarkModel.aggregate([
+      {
+        $match: {
+          // select from owner only
+          owner: Types.ObjectId(search.owner),
+        },
+      },
+      {
+        $unwind: {
+          path: '$tags',
+          preserveNullAndEmptyArrays: true,
+        },
+      },
+      {
+        // join
+        $lookup: {
+          from: 'tags',
+          localField: 'tags',
+          foreignField: '_id',
+          as: 'tags',
+        },
+      },
+      {
+        // partial text search, all fields
+        // expensive ops
+        $match: {
+          $or: [
+            {
+              note: { $regex: `${search.text}`, $options: 'i' },
+              tags: { $regex: `${search.text}`, $options: 'i' },
+              domain: { $regex: `${search.text}`, $options: 'i' },
+              original: { $regex: `${search.text}`, $options: 'i' },
+            },
+          ],
+        },
+      },
+    ]);
+
+    return { results };
   }
 }
