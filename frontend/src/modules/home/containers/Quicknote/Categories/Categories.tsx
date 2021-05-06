5f19b986ecacb066de7eff59801d727c97a60a62
sirapitch limpanithiphat <hamzaakkk@hotmail.com>
2021-05-06 07:34:42 +0700

    refactor: edit categories

--- frontend/src/modules/home/containers/Quicknote/Categories/Categories.tsx
+++ frontend/src/modules/home/containers/Quicknote/Categories/Categories.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
-import { Tag } from "antd";
+import { Input,Tag } from "antd";
+import { PlusOutlined } from '@ant-design/icons';
 const { CheckableTag } = Tag;
+
 
 const tags = ["Movies", "Books", "Music", "Sports"];
 
@@ -9,19 +11,38 @@ const Categories: React.FC = () => {
     {
       label: string;
       checked: boolean;
+      inputVisible: boolean;
+      editInputIndex: -1;
+      editInputValue: '';
     }[]
   >([]);
+  const [inputValue, setInputValue] = useState({inputVal: ""});
 
   const onCheck = (label: string) => {
     return (checked: boolean) => {
       setTags((s) =>
-        s.map((el: { label: string; checked: any }) => {
+        s.map((el: { label: string; checked: any;inputVisible: any; editInputIndex: any; editInputValue: any;  }) => {
           if (el.label === label) return { ...el, check: !el.checked };
           return el;
         })
       );
     };
   };
+  // const showInput = () => {
+  //   setTags({ inputVisible: true });
+  // };
+
+
+
+  // const handleInputChange = (e) => {
+  //   setInputValue({ inputVal: e.target.value });
+  // };
+
+  // const handleClose = (removedTag: any) => {
+  //   const tags = tags.checked.filter(tag => tag !== removedTag);
+  //   console.log(tags);
+  //   setState({ tags });
+  // };
 
   return (
     <>
@@ -32,10 +53,30 @@ const Categories: React.FC = () => {
             key={tag.label || ""}
             checked={tag.checked}
             onChange={onCheck(tag.label)}
+            // closable={index !== 0}
+            // onClose={() => handleClose(tag)}
           >
             {tag}
           </CheckableTag>
         ))}
+        {/* {inputVisible && (
+          <Input
+            ref={this.saveInputRef}
+            type="text"
+            size="small"
+            value={inputValue.inputVal}
+            onChange={handleInputChange}
+            // onBlur={this.handleInputConfirm}
+            // onPressEnter={this.handleInputConfirm}
+          />
+        )} */}
+        {/* {!inputVisible && (
+          <Tag onClick={showInput}>
+            <PlusOutlined /> New Tag
+          </Tag>
+        )} */}
+
+        
       </div>
     </>
   );
