986197cd5a28db198d418dd0f40fef5cdfe71e01
yee2542 <yee2542gtl@gmail.com>
2021-05-07 12:11:50 +0700

    add data recent

--- frontend/src/modules/home/containers/Bookmark/index.tsx
+++ frontend/src/modules/home/containers/Bookmark/index.tsx
@@ -5,10 +5,10 @@ import {
   BookmarkEditInputDto,
   useEditBookmarkByIdMutation,
   useGetBookmarkByTagsQuery,
+  useGetRecentBookmarkQuery,
 } from "../../../../common/services/generate/generate-types";
 import { Store } from "../../../../common/store";
 import Cards from "../../components/Cards/Cards";
-import { MOCK_TAGS } from "../../components/Tags";
 
 const BookmarkContainer: React.FC = () => {
   const userId = useSelector((s: Store) => s.user.user._id);
@@ -20,6 +20,12 @@ const BookmarkContainer: React.FC = () => {
         owner: userId,
         tags: [bookmarkTags],
       },
+    },
+  });
+
+  const { data: recent } = useGetRecentBookmarkQuery({
+    variables: {
+      owner: userId,
     },
   });
 
@@ -35,7 +41,6 @@ const BookmarkContainer: React.FC = () => {
   });
 
   const onEdit = (data: BookmarkEditInputDto) => {
-    console.log("onEdit - card", data);
     editBookmarkByIdMutation({
       variables: {
         bookmark: { ...data, original: data.original },
@@ -47,6 +52,25 @@ const BookmarkContainer: React.FC = () => {
     <div style={{ paddingTop: "16px" }}>
       {loading && <p>loading ...</p>}
       <Row>
+        {/* only recent */}
+        {bookmarkTags === "recent" &&
+          recent?.getRecentBookmark.map((el) => (
+            <Col span={8} key={el._id}>
+              <Cards
+                _id={el._id}
+                domain={el.domain}
+                original={el.original}
+                note={el.note}
+                tags={el.tags.map((t) => ({
+                  _id: "",
+                  label: t,
+                  checked: true,
+                }))}
+                onEdit={onEdit}
+              />
+            </Col>
+          ))}
+
         {data?.searchFilterText.results.map((el) => (
           <Col span={8} key={el._id}>
             <Cards
@@ -54,7 +78,7 @@ const BookmarkContainer: React.FC = () => {
               domain={el.domain}
               original={el.original}
               note={el.note}
-              tags={MOCK_TAGS}
+              tags={el.tags.map((t) => ({ _id: "", label: t, checked: true }))}
               onEdit={onEdit}
             />
           </Col>
