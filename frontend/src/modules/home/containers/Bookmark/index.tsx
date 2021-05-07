929492c6fd7e07df00a7faf8d4d916a93c7d6ea6
yee2542 <yee2542gtl@gmail.com>
2021-05-07 11:16:41 +0700

    wip binding bookmark query

--- frontend/src/modules/home/containers/Bookmark/index.tsx
+++ frontend/src/modules/home/containers/Bookmark/index.tsx
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import {
+  BookmarkEditInputDto,
+  useGetBookmarkByTagsQuery,
+} from "../../../../common/services/generate/generate-types";
+import { Store } from "../../../../common/store";
+import Cards from "../../components/Cards/Cards";
+import { MOCK_TAGS } from "../../components/Tags";
 
 const BookmarkContainer: React.FC = () => {
-  return <div></div>;
+  const userId = useSelector((s: Store) => s.user.user._id);
+  const bookmarkTags = useSelector((s: Store) => s.bookmark.selectedTag);
+
+  const { data, loading, error } = useGetBookmarkByTagsQuery({
+    variables: {
+      opts: {
+        owner: userId,
+        tags: [bookmarkTags],
+      },
+    },
+  });
+
+  const onEdit = (data: BookmarkEditInputDto) => {};
+
+  return (
+    <div>
+      {loading && <p>loading ...</p>}
+      {data?.searchFilterText.results.map((el) => (
+        <Cards
+          _id={el._id}
+          domain={el.domain}
+          original={el.original}
+          note={el.note}
+          tags={MOCK_TAGS}
+          onEdit={onEdit}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default BookmarkContainer;
