734e7869f5877f7dbf68019aaf1fc0be4d0640fc
yee2542 <korbboon.thuswongsa@mail.kmutt.ac.th>
2021-05-07 07:09:48 +0700

    [FIX-31] Fix AccountBadge on-click outside (#35)
    * add comment
    
    * fix bad click logic outside
    
    * fix bad story
    
    * add bypass auth

--- frontend/src/pages/index.stories.tsx
+++ frontend/src/pages/index.stories.tsx
@@ -1,12 +1,21 @@
+import { ApolloProvider } from "@apollo/client";
 import { Meta, Story } from "@storybook/react";
 import React from "react";
-import IndexPage from ".";
+import { Provider } from "react-redux";
+import IndexPage, { IndexPageProps } from ".";
+import { apolloClient } from "../common/services/apollo.client";
+import { store } from "../common/store";
 
 export default {
   title: "Pages/Home",
   component: IndexPage,
 } as Meta;
 
-const IndexPageStory: Story = (args) => <IndexPage {...args} />;
-
+const IndexPageStory: Story<IndexPageProps> = (args) => (
+  <Provider store={store}>
+    <ApolloProvider client={apolloClient}>
+      <IndexPage {...args} enableAuth={false} />
+    </ApolloProvider>
+  </Provider>
+);
 export const Default = IndexPageStory.bind({});
