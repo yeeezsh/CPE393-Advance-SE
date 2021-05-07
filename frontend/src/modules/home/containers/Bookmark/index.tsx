b58af55c5c7063ad2f7e69dff37ece4f0f30f89e
yee2542 <yee2542gtl@gmail.com>
2021-05-07 11:58:52 +0700

    bind

--- frontend/src/modules/home/containers/Bookmark/index.tsx
+++ frontend/src/modules/home/containers/Bookmark/index.tsx
@@ -1,7 +1,9 @@
-import React, { useEffect } from "react";
+import { Col, Row } from "antd";
+import React from "react";
 import { useSelector } from "react-redux";
 import {
   BookmarkEditInputDto,
+  useEditBookmarkByIdMutation,
   useGetBookmarkByTagsQuery,
 } from "../../../../common/services/generate/generate-types";
 import { Store } from "../../../../common/store";
@@ -12,7 +14,7 @@ const BookmarkContainer: React.FC = () => {
   const userId = useSelector((s: Store) => s.user.user._id);
   const bookmarkTags = useSelector((s: Store) => s.bookmark.selectedTag);
 
-  const { data, loading, error } = useGetBookmarkByTagsQuery({
+  const { data, loading } = useGetBookmarkByTagsQuery({
     variables: {
       opts: {
         owner: userId,
@@ -21,21 +23,43 @@ const BookmarkContainer: React.FC = () => {
     },
   });
 
-  const onEdit = (data: BookmarkEditInputDto) => {};
+  const [editBookmarkByIdMutation] = useEditBookmarkByIdMutation({
+    variables: {
+      bookmark: {
+        _id: "",
+        original: "",
+        note: "",
+        tags: [],
+      },
+    },
+  });
+
+  const onEdit = (data: BookmarkEditInputDto) => {
+    console.log("onEdit - card", data);
+    editBookmarkByIdMutation({
+      variables: {
+        bookmark: { ...data, original: data.original },
+      },
+    });
+  };
 
   return (
-    <div>
+    <div style={{ paddingTop: "16px" }}>
       {loading && <p>loading ...</p>}
-      {data?.searchFilterText.results.map((el) => (
-        <Cards
-          _id={el._id}
-          domain={el.domain}
-          original={el.original}
-          note={el.note}
-          tags={MOCK_TAGS}
-          onEdit={onEdit}
-        />
-      ))}
+      <Row>
+        {data?.searchFilterText.results.map((el) => (
+          <Col span={8} key={el._id}>
+            <Cards
+              _id={el._id}
+              domain={el.domain}
+              original={el.original}
+              note={el.note}
+              tags={MOCK_TAGS}
+              onEdit={onEdit}
+            />
+          </Col>
+        ))}
+      </Row>
     </div>
   );
 };
