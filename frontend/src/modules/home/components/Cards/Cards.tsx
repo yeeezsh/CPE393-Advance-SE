0296b41c3f1e87d8ee4bf506efd420c0b8bf3a6b
yee2542 <yee2542gtl@gmail.com>
2021-05-07 09:09:55 +0700

    add edit box

--- frontend/src/modules/home/components/Cards/Cards.tsx
+++ frontend/src/modules/home/components/Cards/Cards.tsx
@@ -1,6 +1,6 @@
-import { Card, Tag } from "antd";
-import React from "react";
-import { TagType } from "../Tags/index";
+import { Button, Card, Input, Tag } from "antd";
+import React, { useState } from "react";
+import Tags, { TagType } from "../Tags/index";
 
 const { Meta } = Card;
 
@@ -10,20 +10,52 @@ export interface CardProps {
   note: string;
 }
 
-const Cards: React.FC<CardProps> = (props) => {
-  return (
-    <>
-      <Card hoverable>
-        <Meta title={props.domain} description={props.note} />
-        <div style={{ height: "12px" }} />
+const VisibleCard: React.FC<CardProps & { onClick: () => void }> = (props) => (
+  <div onClick={props.onClick}>
+    <Meta title={props.domain} description={props.note} />
+    <div style={{ height: "12px" }} />
 
-        {/* tags */}
-        {props.tags.map((el) => (
-          <Tag style={{ minWidth: "3em", textAlign: "center" }}>{el.label}</Tag>
-        ))}
-      </Card>
-      ,
-    </>
+    {/* tags */}
+    {props.tags.map((el) => (
+      <Tag key={el._id} style={{ minWidth: "3em", textAlign: "center" }}>
+        {el.label}
+      </Tag>
+    ))}
+  </div>
+);
+
+const ExpandCard: React.FC<
+  CardProps & { visible: boolean; onSave: () => void }
+> = (props) => {
+  return (
+    <div>
+      <Input value={props.domain} />
+      <div style={{ height: "12px" }} />
+
+      {/* tags */}
+      <Tags tags={props.tags} />
+      <Button onClick={() => props.onSave()}>Save</Button>
+    </div>
+  );
+};
+
+const Cards: React.FC<CardProps> = (props) => {
+  const [expand, setExpand] = useState<boolean>(false);
+
+  const onSave = () => {
+    setExpand(false);
+    console.log("on save", expand);
+  };
+
+  const onClick = () => {
+    setExpand(() => true);
+  };
+
+  return (
+    <Card hoverable>
+      {!expand && <VisibleCard {...props} onClick={onClick} />}
+      {expand && <ExpandCard {...props} visible={expand} onSave={onSave} />}
+    </Card>
   );
 };
 export default Cards;
