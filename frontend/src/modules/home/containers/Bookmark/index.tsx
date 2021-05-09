129805345aef0d9eaa2b92e541ed31f41db15583
yee2542 <yee2542gtl@gmail.com>
2021-05-10 03:33:11 +0700

    fix import

--- frontend/src/modules/home/containers/Bookmark/index.tsx
+++ frontend/src/modules/home/containers/Bookmark/index.tsx
@@ -10,16 +10,21 @@ import {
 import { Store } from "../../../../common/store";
 import Add from "../../components/Add";
 import Cards from "../../components/Cards/Cards";
+import { TagType } from "../../components/Tags";
 
 const BookmarkContainer: React.FC = () => {
   const userId = useSelector((s: Store) => s.user.user._id);
-  const bookmarkTags = useSelector((s: Store) => s.bookmark.selectedTag);
+  const selectedBookmarkTags = useSelector(
+    (s: Store) => s.bookmark.selectedTag
+  );
+
+  const allTags = useSelector((s: Store) => s.tags.tags);
 
   const { data, loading } = useGetBookmarkByTagsQuery({
     variables: {
       opts: {
         owner: userId,
-        tags: [bookmarkTags],
+        tags: [selectedBookmarkTags],
       },
     },
   });
@@ -45,7 +50,7 @@ const BookmarkContainer: React.FC = () => {
   const onEdit = (data: BookmarkEditInputDto) => {
     editBookmarkByIdMutation({
       variables: {
-        bookmark: { ...data, original: data.original },
+        bookmark: { ...data, original: data.original, tags: data.tags },
       },
     });
   };
@@ -66,25 +71,35 @@ const BookmarkContainer: React.FC = () => {
       </Row>
 
       {loading && <p>loading ...</p>}
+
       <Row>
         {/* only recent */}
-        {bookmarkTags === "recent" &&
-          recent?.getRecentBookmark.map((el) => (
-            <Col span={8} key={el._id}>
-              <Cards
-                _id={el._id}
-                domain={el.domain}
-                original={el.original}
-                note={el.note}
-                tags={el.tags.map((t) => ({
-                  _id: "",
-                  label: t,
-                  checked: true,
-                }))}
-                onEdit={onEdit}
-              />
-            </Col>
-          ))}
+        {selectedBookmarkTags === "recent" &&
+          allTags &&
+          recent?.getRecentBookmark.map((el) => {
+            const mappedTags = el.tags.map((el) => {
+              const foundTag = allTags.find((f) => f._id === el);
+              return {
+                _id: el,
+                checked: true,
+                label: foundTag?.label,
+                createAt: foundTag?.createAt,
+              };
+            }) as TagType;
+
+            return (
+              <Col span={8} key={el._id}>
+                <Cards
+                  _id={el._id}
+                  domain={el.domain}
+                  original={el.original}
+                  note={el.note}
+                  tags={mappedTags}
+                  onEdit={onEdit}
+                />
+              </Col>
+            );
+          })}
 
         {data?.searchFilterText.results.map((el) => (
           <Col span={8} key={el._id}>
@@ -93,7 +108,12 @@ const BookmarkContainer: React.FC = () => {
               domain={el.domain}
               original={el.original}
               note={el.note}
-              tags={el.tags.map((t) => ({ _id: "", label: t, checked: true }))}
+              tags={el.tags.map((t) => ({
+                _id: "",
+                label: t,
+                checked: true,
+                createAt: new Date(),
+              }))}
               onEdit={onEdit}
             />
           </Col>
