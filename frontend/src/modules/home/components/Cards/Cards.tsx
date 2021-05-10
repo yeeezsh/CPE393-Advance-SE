db021a6652871bff4cc97d48c3740761400cc032
yee2542 <yee2542gtl@gmail.com>
2021-05-10 08:36:18 +0700

    able to to change tag

--- frontend/src/modules/home/components/Cards/Cards.tsx
+++ frontend/src/modules/home/components/Cards/Cards.tsx
@@ -5,6 +5,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { BookmarkEditInputDto } from "../../../../common/services/generate/generate-types";
 import { Store } from "../../../../common/store";
 import { BookmarkAction } from "../../../../common/store/bookmark";
+import { DataTagStore } from "../../../../common/store/tags";
 import Tags, { TagType } from "../Tags/index";
 import {
   CardInputUrlStyle,
@@ -37,12 +38,25 @@ const VisibleCard: React.FC<CardProps & { onClick: () => void }> = (props) => (
   </div>
 );
 
+// EDIT CARD
 const ExpandCard: React.FC<
   CardProps & { visible: boolean; onSave: (save: BookmarkEditInputDto) => void }
 > = (props) => {
   const dispatch = useDispatch();
+  const alltags = useSelector((s: Store) => s.tags.tags);
   const [original, setOriginal] = useState(props.domain);
   const [note, setNote] = useState(props.note);
+
+  const selectedTags = props.tags.map((el) => el._id);
+  const initTagsMapped = alltags.map((el) => ({
+    ...el,
+    checked: selectedTags.includes(el._id),
+  }));
+  console.log("selectedTags", selectedTags);
+  console.log("initTagsMapped", initTagsMapped);
+
+  const [tags, setTags] = useState(initTagsMapped);
+  console.log("selectingTags", tags);
 
   const onSave = (s: boolean) => {
     dispatch(BookmarkAction.setUnSelectingBookmark());
@@ -52,6 +66,7 @@ const ExpandCard: React.FC<
         _id: props._id,
         original: original,
         note,
+        tags: tags.filter((el) => el.checked).map((el) => el._id),
       });
 
     //   save the same
@@ -70,6 +85,23 @@ const ExpandCard: React.FC<
 
   const onOk = () => {
     onSave(true);
+  };
+
+  const onDelete = () => {};
+
+  const onSelectTag = (tagId: string) => {
+    setTags((t) => {
+      return t.map((el) => {
+        const isSelecting = el._id === tagId;
+        if (isSelecting) {
+          return {
+            ...el,
+            checked: !el.checked,
+          };
+        }
+        return el;
+      });
+    });
   };
 
   return (
@@ -104,10 +136,29 @@ const ExpandCard: React.FC<
         onChange={(e) => setNote(() => e.target.value)}
       />
       <div style={{ height: "4px" }} />
-      <Tags tags={props.tags} />
+      <Tags
+        tags={tags.map((el) => ({
+          ...el,
+          createAt: new Date(el.createAt),
+        }))}
+        onSelect={onSelectTag}
+        onAddNewTag={(newTag) =>
+          setTags((t) => [
+            ...t,
+            {
+              _id: newTag._id,
+              checked: true,
+              createAt: newTag.createAt,
+              label: newTag.label,
+            },
+          ])
+        }
+      />
     </Modal>
   );
 };
+
+// MAIN CARD
 
 const Cards: React.FC<CardProps> = (props) => {
   const [expand, setExpand] = useState<boolean>(false);
@@ -119,6 +170,8 @@ const Cards: React.FC<CardProps> = (props) => {
     setExpand(false);
     props.onEdit && props.onEdit(data);
   };
+
+  console.log("props.tags", props.tags);
 
   const onClick = () => {
     setExpand(() => true);
