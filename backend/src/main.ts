2b8fbfdb0c18f06b88f71179382aea533675ce25
yee2542 <yee2542gtl@gmail.com>
2021-05-10 11:46:50 +0700

    fix deps

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,11 +8,17 @@ import { ConfigModule } from './config/config.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
-  const { port }: ConfigAppServiceType = app
+  const { port, origin }: ConfigAppServiceType = app
     .select(ConfigModule)
     .get(ConfigAppService)
     .get();
+  app.useGlobalPipes(new ValidationPipe());
+  app.setGlobalPrefix('/api');
+  app.enableCors({
+    origin: [origin],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
 
   app.use(cookieParser());
 
